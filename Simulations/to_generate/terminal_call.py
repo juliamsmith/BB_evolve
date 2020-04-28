0026168ce1d3a7ba3dbf6f7d23d565f7f1862be9
juliamsmith <jmsmith20@uchicago.edu>
2020-04-28 00:00:47 -0400

    no major changes, preparing to do more runs

--- Simulations/to_generate/terminal_call.py
+++ Simulations/to_generate/terminal_call.py
@@ -28,37 +28,28 @@ if __name__ == "__main__":
 #     #        vec=vec+[dist/3*male+quot*dist/10]
      
         
-#     #ADJUST
-#     change_what_vec = ['pos', 'strat']
-   
-#     pos_interval_vec = [.1] 
-#     strat_interval_vec = [.75] #I THINK .75 MAKES SENSE! 
-
-#     sd_adjust_vec = [1,0] #could re-write code to allow degree of adjustment based on sd below to vary
+#ADJUST    
     
-    
-#     males_vec = [6]
-#     dist_mult_vec = [900]
-#     male_strat_vec = ['All.VHigh'] #I think this may drive selection more strongly... this whole thing is a bit of a best case scenario for sig
-#     male_pos_vec = ['EvenTenthClumped']
-#     change_what_vec = ['pos']
-#     pos_interval_vec = [.1] #now scaled TO THE SIZE OF THE TOTAL ENVIRONMENT
-#     strat_interval_vec = []
-#     sd_adjust_vec = [1,0]
-#     sims = range(5)
+#     males_vec = [6,12]
+#     dist_mult_vec = [300, 600, 900]
+#     male_strat_vec = ['Alt.Low.VHigh'] #I think this may drive selection more strongly... this whole thing is a bit of a best case scenario for sig
+#     male_pos_vec = ['Uniform']
+#     change_what_vec = ['strat']
+#     pos_interval_vec = [] #now scaled TO THE SIZE OF THE TOTAL ENVIRONMENT
+#     strat_interval_vec = [.75]
+#     sd_adjust_vec = [0,1]
+#     sims = range(10)
 #     num_gens = 2000
-    
-    
     males_vec = [6]
-    dist_mult_vec = [900]
-    male_strat_vec = ['All.High'] #I think this may drive selection more strongly... this whole thing is a bit of a best case scenario for sig
-    male_pos_vec = ['UniformJittered']
+    dist_mult_vec = [300]
+    male_strat_vec = ['Alt.Low.VHigh'] #I think this may drive selection more strongly... this whole thing is a bit of a best case scenario for sig
+    male_pos_vec = ['Uniform']
     change_what_vec = ['strat']
     pos_interval_vec = [] #now scaled TO THE SIZE OF THE TOTAL ENVIRONMENT
     strat_interval_vec = [.75]
     sd_adjust_vec = [1]
     sims = range(2)
-    num_gens = 100
+    num_gens = 10
     
     vary_params(males_vec, dist_mult_vec, male_strat_vec, male_pos_vec, change_what_vec, pos_interval_vec, strat_interval_vec, sd_adjust_vec, sims, num_gens)
     
--- Simulations/to_run/0_null_strat_0.75_1_All.High_UniformJittered_900_6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -J 0strat_0.75_1_All.High_UniformJittered_900_6
-#SBATCH --time=00:30:00
-#SBATCH -p broadwl
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-python3 null_bowerbird_prog.py ../to_store/strat_0.75_1_All.High_UniformJittered_900_6/parameters/in_00_strat_0.75_1_All.High_UniformJittered_900_6.csv
-mv null_00_strat_0.75_1_All.High_UniformJittered_900_6.csv ../to_store/strat_0.75_1_All.High_UniformJittered_900_6/nulls/null_00_strat_0.75_1_All.High_UniformJittered_900_6.csv
