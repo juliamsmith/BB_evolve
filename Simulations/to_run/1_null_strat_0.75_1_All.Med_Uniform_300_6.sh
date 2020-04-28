24aacd23f0f2c33bf0aa91d928f9cdb4c15b671b
juliamsmith <jmsmith20@uchicago.edu>
2020-04-28 00:49:19 -0400

    no big changes

---
+++ Simulations/to_run/1_null_strat_0.75_1_All.Med_Uniform_300_6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -J 1strat_0.75_1_All.Med_Uniform_300_6
+#SBATCH --time=00:30:00
+#SBATCH -p broadwl
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+python3 null_bowerbird_prog.py ../to_store/strat_0.75_1_All.Med_Uniform_300_6/parameters/in_01_strat_0.75_1_All.Med_Uniform_300_6.csv
+mv null_01_strat_0.75_1_All.Med_Uniform_300_6.csv ../to_store/strat_0.75_1_All.Med_Uniform_300_6/nulls/null_01_strat_0.75_1_All.Med_Uniform_300_6.csv
