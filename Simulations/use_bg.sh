bbec5ebcd43780e04cadfc634f076971de64c9b5
juliamsmith <jmsmith20@uchicago.edu>
2020-04-29 11:00:56 -0400

    results from the most recent runs (strat, alt.low.vhigh, varying distances)

--- Simulations/use_bg.sh
+++ Simulations/use_bg.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 printf "BACKGROUND RUNS STARTED\n"
+
+
+function max2 {
+   while [ `jobs | wc -l` -ge 45 ]
+   do
+      sleep 5
+   done
+}
+
 cd to_run
 for f in *.sh; do
-    ./$f &
+    max2; ./$f &
 done
+wait
+echo "All done!!!"
